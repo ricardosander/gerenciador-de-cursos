017e331a129be7f3e68847f05ee88f19a015e1f4
ricardosander <ricardo.sander.lopes@gmail.com>
2016-08-02 00:19:42 -0300

    Usando forEach e lambda do Java 8.

--- TestandoListas.java
+++ TestandoListas.java
@@ -3,26 +3,30 @@ import java.util.ArrayList;
 public class TestandoListas {
 
 	public static void main(String[] args) {
-		
+
 		String aula1 = "Modelando a classe Aula";
 		String aula2 = "Conhecendo mais de listas";
 		String aula3 = "Trabalhando com Cursos e Sets";
-		
+
 		ArrayList<String> aulas = new ArrayList<>();
 		aulas.add(aula1);
 		aulas.add(aula2);
 		aulas.add(aula3);
 
-		for (String aula: aulas) {
+		for (String aula : aulas) {
 			System.out.println("Aula: " + aula);
 		}
-		
+
 		System.out.println("Removendo primeiro elemento:");
 		aulas.remove(0);
-		
 
-		for (int  i = 0; i < aulas.size(); i++) {
-			System.out.println("Aula "+ i +": " + aulas.get(i));
+		for (int i = 0; i < aulas.size(); i++) {
+			System.out.println("Aula " + i + ": " + aulas.get(i));
 		}
+
+		aulas.forEach(aula -> {
+			System.out.println("Aula:");
+			System.out.println(aula);
+		});
 	}
 }
