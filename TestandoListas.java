6002122828086ed5724f932eb3564dcb72b45495
ricardosander <ricardo.sander.lopes@gmail.com>
2016-08-02 00:30:43 -0300

    Módulo 1, Exercício 1: iniciando com ArrayList.

--- TestandoListas.java
+++ TestandoListas.java
@@ -1,36 +1,20 @@
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.List;
 
 public class TestandoListas {
 
 	public static void main(String[] args) {
 
-		String aula1 = "Modelando a classe Aula";
-		String aula2 = "Conhecendo mais de listas";
-		String aula3 = "Trabalhando com Cursos e Sets";
+		String curso1 = "Java 8";
+		String curso2 = "Servelets: Fundamentos de Java na Web";
+		String curso3 = "Designer Ptters";
 
-		ArrayList<String> aulas = new ArrayList<>();
-		aulas.add(aula1);
-		aulas.add(aula2);
-		aulas.add(aula3);
+		List<String> cursos = new ArrayList<>();
+		cursos.add(curso1);
+		cursos.add(curso2);
+		cursos.add(curso3);
 
-		for (String aula : aulas) {
-			System.out.println("Aula: " + aula);
-		}
-		
-		System.out.println("Ordenando lista:");
-		Collections.sort(aulas);
-		aulas.forEach(aula -> {
-			System.out.println("Aula:");
-			System.out.println(aula);
-		});
-		
-		System.out.println("Removendo primeiro elemento:");
-		aulas.remove(0);
-
-		for (int i = 0; i < aulas.size(); i++) {
-			System.out.println("Aula " + i + ": " + aulas.get(i));
-		}
+		System.out.println(cursos);
 
 	}
 }
