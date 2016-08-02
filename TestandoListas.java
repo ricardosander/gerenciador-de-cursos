dd996b1223826c569b8f95b04f72c9df8dcff1f6
ricardosander <ricardo.sander.lopes@gmail.com>
2016-08-02 00:10:37 -0300

    Iniciando com ArrayList

---
+++ TestandoListas.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class TestandoListas {
+
+	public static void main(String[] args) {
+		
+		String aula1 = "Modelando a classe Aula";
+		String aula2 = "Conhecendo mais de listas";
+		String aula3 = "Trabalhando com Cursos e Sets";
+		
+		ArrayList<String> aulas = new ArrayList<>();
+		aulas.add(aula1);
+		aulas.add(aula2);
+		aulas.add(aula3);
+		
+		System.out.println(aulas);
+	}
+}
