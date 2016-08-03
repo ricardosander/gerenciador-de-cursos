b79403048b114c2cc936fb0e33dd2242eca9456c
ricardosander <ricardo.sander.lopes@gmail.com>
2016-08-02 22:46:13 -0300

    Módulo 3, Exercício 2: adicionando novas aulas em um curso.

--- TestaCurso.java
+++ TestaCurso.java
@@ -1,9 +1,18 @@
+import java.util.List;
 
 public class TestaCurso {
 
 	public static void main(String[] args) {
 
 		Curso curso = new Curso("Curso de Java", "Ricardo");
-		System.out.println(curso.getAulas());
+
+		List<Aula> aulas = curso.getAulas();
+		System.out.println(aulas);
+
+		aulas.add(new Aula("Aula 1", 10));
+		aulas.add(new Aula("Aula 2", 20));
+		aulas.add(new Aula("Aula 3", 30));
+
+		System.out.println(aulas);
 	}
 }
