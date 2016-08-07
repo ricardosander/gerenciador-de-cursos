6a3cbb91304d81be17c71c1e60ff0f9328ebb429
ricardosander <ricardo.sander.lopes@gmail.com>
2016-08-06 22:30:22 -0300

    Módulo 7, Exercício 3: sobreescrevendo hashCode e equals segundo o contrato da classe Object (com ajuda do Eclipse).

--- br/com/alura/Aluno.java
+++ br/com/alura/Aluno.java
@@ -22,18 +22,35 @@ public class Aluno {
 		return this.matricula;
 	}
 	
+	
 	@Override
 	public int hashCode() {
-		return this.nome.hashCode();
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + matricula;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
-		
-		Aluno outroAluno = (Aluno) obj;
-		return this.nome.equals(outroAluno.getNome());
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		if (matricula != other.matricula)
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "[Nome: " + this.nome + ", matrícula: " + this.matricula + "]";
